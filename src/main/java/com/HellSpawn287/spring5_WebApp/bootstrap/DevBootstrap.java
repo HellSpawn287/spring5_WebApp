44f5e3916ba982a2eafcf9895113029a811216ae
Robert Prus <robert.prus87@gmail.com>
2018-09-22 18:45:03 +0200

    Add DevVootstrap class Add initData()  method Used spring @Component

---
+++ src/main/java/com/HellSpawn287/spring5_WebApp/bootstrap/DevBootstrap.java
@@ -0,0 +1,45 @@
+package com.HellSpawn287.spring5_WebApp.bootstrap;
+
+import com.HellSpawn287.spring5_WebApp.model.Author;
+import com.HellSpawn287.spring5_WebApp.model.Book;
+import com.HellSpawn287.spring5_WebApp.repository.AuthorRepository;
+import com.HellSpawn287.spring5_WebApp.repository.BookRepository;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> {
+
+    private AuthorRepository authorRepository;
+    private BookRepository bookRepository;
+
+    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        initData();
+    }
+
+    private void initData() {
+
+        //Eric
+        Author eric = new Author("Eric", "Evans");
+        Book ddd = new Book("Domain Driven Development", "1234", "Harper Collins");
+        eric.getBooks().add(ddd);
+        ddd.getAuthors().add(eric);
+
+        authorRepository.save(eric);
+        bookRepository.save(ddd);
+
+        Author rod = new Author("Rod", "Johnson");
+        Book noEJB = new Book("J2EE Development without EJB", "23444", "Worx");
+        rod.getBooks().add(noEJB);
+
+        authorRepository.save(rod);
+        bookRepository.save(noEJB);
+    }
+}
